ce022eb9818602e24cd4b0ef8bc9fbb8077bea7b
AlexBoronin <alex.boronin.22@gmail.com>
2024-07-17 00:52:36 +0300

    feat: some effects

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,30 +1,54 @@
-/**todo селект с анимацией*/
-const select = document.querySelector('.select');
-const selectOption = document.querySelectorAll('.select-option');
-const inside = document.querySelector('.inside')
-select.addEventListener('click', (e) => {
-  if (select.classList.contains('open')) {
-    select.classList.remove('open');
-    inside.style.maxHeight = `0`;
-    setTimeout(() => {
-      inside.classList.remove('see');
-    }, 250)
+(function () {
+  /** стилизация хедера при скролле */
+  const header = document.querySelector('.header');
+  window.onscroll = () => {
+    if (window.scrollY > 40) {
+      header.classList.add('scrolly');
+    } else {
+      header.classList.remove('scrolly');
+    }
+  };
 
-  } else {
-
-    inside.style.maxHeight = `${inside.scrollHeight}px`;
-    inside.classList.add('see');
-    select.classList.toggle('open');
-  }
-});
-selectOption.forEach(elem => {
-  elem.addEventListener('click', (e) => {
-    inside.style.maxHeight = `0`;
-    setTimeout(() => {
-      select.innerText = e.target.innerText;
+  /** кастомный анимированный селект */
+  const select = document.querySelector('.select');
+  const selectOption = document.querySelectorAll('.select-option');
+  const inside = document.querySelector('.inside')
+  select.addEventListener('click', (e) => {
+    if (select.classList.contains('open')) {
       select.classList.remove('open');
-      select.style.color = '#333';
-      inside.classList.remove('see')
-    }, 250);
+      inside.style.maxHeight = `0`;
+      setTimeout(() => {
+        inside.classList.remove('see');
+      }, 250)
+
+    } else {
+
+      inside.style.maxHeight = `${inside.scrollHeight}px`;
+      inside.classList.add('see');
+      select.classList.toggle('open');
+    }
   });
-});
+  selectOption.forEach(elem => {
+    elem.addEventListener('click', (e) => {
+      inside.style.maxHeight = `0`;
+      setTimeout(() => {
+        select.innerText = e.target.innerText;
+        select.classList.remove('open');
+        select.style.color = '#333';
+        inside.classList.remove('see')
+      }, 250);
+    });
+  });
+
+  /** выбор пункта меню навигации */
+  const navMenu = document.querySelector('.nav');
+  const navLinkElements = document.querySelectorAll('.nav__link');
+  navMenu.addEventListener('click', (e) => {
+    if(!e.target.classList.contains('active')){
+      navLinkElements.forEach(elem => {
+        elem.classList.remove('active');
+      });
+      e.target.classList.add('active');
+    }
+  });
+}());
